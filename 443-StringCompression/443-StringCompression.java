ddc423da8d05b14414981d86bdb191d9ebb1cff9
Ravi Mahajan <ravidnya@gmail.com>
2025-11-05 20:13:27 -0600

    Create 443-StringCompression

---
+++ 443-StringCompression/443-StringCompression.java
@@ -0,0 +1,27 @@
+// Last updated: 11/5/2025, 8:13:27 PM
+class Solution {
+    public int compress(char[] chars) {
+        int n = chars.length;
+        int index = 0;
+
+        for(int i = 0; i < n; i++){
+            char ch = chars[i];
+            int count = 0;
+            while(i < n && chars[i]==ch){
+                count++;
+                i++;
+            }
+
+            if(count == 1){
+                chars[index++] = ch;
+            } else {
+                chars[index++] = ch;
+                for(char digit: Integer.toString(count).toCharArray()){
+                    chars[index++] = digit;
+                }
+            }
+            i--;
+        }
+        return index;
+    }
+}
